ebb2077913b609ee3e8dcc3db9d208ca20297860
JayJSYeo <48149355+JayJSYeo@users.noreply.github.com>
2019-06-08 18:13:32 +0900

    Add files via upload

---
+++ 조도센서.ino
@@ -0,0 +1,38 @@
+void setup() {
+  // put your setup code here, to run once:
+
+  Serial.begin (9600);
+
+}
+
+void loop() {
+  // put your main code here, to run repeatedly:
+
+  int valueleft ;
+  int valueright ;
+  int difference;
+  
+  valueleft = analogRead(A0) ;
+  valueright = analogRead(A1) ;
+
+  //Serial.println(valueleft) ;
+  //Serial.println(valueright) ;
+
+  difference=valueleft-valueright;
+  //Serial.println(difference);
+
+ 
+  if( difference <-30 ){
+    Serial.println("left");
+  }else if( difference<30){
+    Serial.println("center") ;
+  }else{
+    Serial.println("right") ;
+  }
+  
+ 
+   delay(500) ;
+
+  
+
+}
